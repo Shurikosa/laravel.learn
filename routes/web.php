f60fa6a5cbe8fe8aa3c14e89bb6739d1e8fd4d20
Oleksandr Osaula <shurikosaula@gmail.com>
2024-09-05 19:45:23 +0300

    Routs worked

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Route::get('/greet/{name}', function ($name) {
     return "Hello {$name}";
 });
 
-
 /*
  * маршрут, який приймає необов'язковий параметр name і виводить "Hello, {name}!" або "Hello, Guest!",
  * якщо параметр не передано. Наприклад, /greet має виводити "Hello, Guest!".
@@ -30,9 +29,54 @@ Route::get('/greet/{name?}', function ($name = 'guest') {
     return "Hello {$name}";
 });
 /*
- *  маршрут /submit, який приймає дані за допомогою методу POST і повертає повідомлення "Form submitted!".
+ * маршрут /submit, котрий приймає дані за допомогою методу POST і повертає повідомлення "Form submitted!".
+ * для тесту краще використовувати postman
+ * виникає помилка 419. тому цей запит потрібно вимкнути з валідації csrf
  */
+Route::post('/submit', function () {
+   return 'Form Submitted';
+})->withoutMiddleware(\Illuminate\Foundation\Http\Middleware\VerifyCsrfToken::class);
 
+/*
+ * маршрут /update, який використовує метод PUT і повертає повідомлення "Data updated!".
+ */
+Route::put('/update', function () {
+   return 'Form Updated';
+});
 
+/*
+ * маршрут /delete, який використовує метод DELETE і повертає повідомлення "Data deleted!".
+ */
+Route::delete('/delete', function () {
+    return 'Form Deleted';
+});
 
+/*
+ * група маршрутів з префіксом admin.
+ * У цій групі додай маршрут /dashboard, який повертає "Admin Dashboard",
+ * і маршрут /settings, який повертає "Admin Settings".
+ */
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Here must be admin dashboard view';
+    });
+    Route::get('/settings', function () {
+        return 'Here must be admin settings view';
+    });
+});
+/*
+ * маршрут /contact, який повертає текст "Contact Page" де ми задаємо ім'я маршруту - contact.
+ * contact — це ім'я маршруту, яке ви використовуєте для посилань або перенаправлень у вашому додатку.
+ * Підсумок:
+ * URL (/contact) визначає, де в браузері буде доступний маршрут.
+ * Ім'я маршруту (->name('contact')) — це зручний спосіб посилатися на цей маршрут у коді,
+ * що робить його більш гнучким і легким для обслуговування.
+ * Приклад для розуміння
+ *Якщо ви хочете згенерувати посилання на сторінку контакту в якомусь шаблоні або контролері,
+ *замість того щоб писати /contact у вашому коді, ви пишете:
+ * route('contact')
+ */
+Route::get('/contact', function () {
+    return 'Contact Page';
+})->name('contact');
 
